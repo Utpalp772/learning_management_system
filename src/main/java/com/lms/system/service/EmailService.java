dbf42f2b5078073db12ddf5a3610a79b6a713b39
Utpalp772 <utpalp772@gmail.com>
2026-09-16 19:47:13 +0530

    Switch OTP email delivery to Resend

--- src/main/java/com/lms/system/service/EmailService.java
+++ src/main/java/com/lms/system/service/EmailService.java
@@ -1,23 +1,36 @@
 package com.lms.system.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.resend.Resend;
+import com.resend.services.emails.model.CreateEmailOptions;
 
 @Service
 public class EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
+    @Value("${RESEND_API_KEY}")
+    private String resendApiKey;
 
     public void sendOtpEmail(String toEmail, String otpCode, String purposeLabel) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Your OTP Code - " + purposeLabel);
-        message.setText("Your OTP code is: " + otpCode + "\n\nThis code will expire in 10 minutes.\n\n"
-                + "If you did not request this, please ignore this email.");
 
-        mailSender.send(message);
+        Resend resend = new Resend(resendApiKey);
+
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from("LMS <onboarding@resend.dev>")
+                .to(toEmail)
+                .subject("Your OTP Code - " + purposeLabel)
+                .text(
+                    "Your OTP code is: " + otpCode
+                    + "\n\nThis code will expire in 10 minutes."
+                    + "\n\nIf you did not request this, please ignore this email."
+                )
+                .build();
+
+        try {
+            resend.emails().send(params);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send OTP email", e);
+        }
     }
 }
